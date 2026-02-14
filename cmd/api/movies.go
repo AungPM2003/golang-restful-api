2ca827fa680963c999c3386aac8ca8a5e48583cc
Aung Phone Myat <aungphonemyat.usa@gmail.com>
2026-02-13 21:59:25 -0500

    decoding json

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -9,7 +10,35 @@ import (
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	//create a struct for what we expect the input will be
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	// this is decoding with json.New Decoder
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	//this is decoding with json.Un,marshal
+	// body, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	app.serverErrorResponse(w, r, err)
+	// 	return
+	// }
+
+	// err = json.Unmarshal(body, &input)
+	// if err != nil {
+	// 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+	// 	return
+	// }
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
